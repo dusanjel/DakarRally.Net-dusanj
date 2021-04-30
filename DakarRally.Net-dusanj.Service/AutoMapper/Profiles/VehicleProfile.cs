78428ea0c7c2c4c77ec50bc2855064ef2af4cc77
Dusan Jel <dusan_jel@hotmail.com>
2021-04-30 11:53:27 +0200

    MalfunctionType AutoMapper Ignore

--- DakarRally.Net-dusanj.Service/AutoMapper/Profiles/VehicleProfile.cs
+++ DakarRally.Net-dusanj.Service/AutoMapper/Profiles/VehicleProfile.cs
@@ -13,13 +13,17 @@ namespace DakarRally.Net_dusanj.Service.AutoMapper.Profiles
                 .ForMember(dest => dest.MotorcycleType, opts => opts.Ignore())
                 .ForMember(dest => dest.VehicleType, opts => opts.Ignore());
 
-            CreateMap<VehicleDto, Vehicle>();
+            CreateMap<VehicleDto, Vehicle>()
+                .ForMember(dest => dest.MalfunctionType, opts => opts.Ignore());
 
-            CreateMap<VehicleDto, Car>();
+            CreateMap<VehicleDto, Car>()
+                .ForMember(dest => dest.MalfunctionType, opts => opts.Ignore());
 
-            CreateMap<VehicleDto, Truck>();
+            CreateMap<VehicleDto, Truck>()
+                .ForMember(dest => dest.MalfunctionType, opts => opts.Ignore());
 
-            CreateMap<VehicleDto, Motorcycle>();
+            CreateMap<VehicleDto, Motorcycle>()
+                .ForMember(dest => dest.MalfunctionType, opts => opts.Ignore());
         }
     }
 }
